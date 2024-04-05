c048833c10c6a78638cf405dc858213f7a4eca6a
Alex <j.a.utexas@edu>
2024-04-05 15:21:41 -0500

    Add properties to world model, including reward function. Add main script to run training

--- src/world.py
+++ src/world.py
@@ -1,11 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from agent import Agent
+from agent import Pursuer, Evader
 
 class World(object):
     def __init__(self, x_l, x_u, y_l, y_u, 
                  area_x, area_y, area_r, d, 
-                 v_min, v_max, u1_max, u2_max):
+                 v_min, v_max, u1_max, u2_max, 
+                 start_state_pursuer,
+                 start_state_evader,
+                 k):
         self.x_l = x_l # x lower limit
         self.x_u = x_u # x upper limit
         self.y_l = y_l # y lower limit
@@ -17,21 +20,47 @@ class World(object):
 
         self.d = d # capture distance
 
-        self.action_space = list()
-        self.pursuer = Agent(state, v_min, v_max, u1_max, u2_max)
-        self.evader = Agent(state, v_min, v_max, u1_max, u2_max)
+        self.k = 1 # distance reward scaling factor
 
-    def is_terminal(self, state):
+        self.action_space = list()
+        self.pursuer = Pursuer(start_state_pursuer, v_min, v_max, u1_max, u2_max)
+        self.evader = Evader(start_state_evader, v_min, v_max, u1_max, u2_max)
+
+    @property
+    def pursuer_succeeds(self):
         x_p, y_p = self.pursuer.get_position()
         x_e, y_e = self.evader.get_position()
+        return (x_p - x_e) ** 2 + (y_p - y_e) ** 2 <= self.d ** 2
 
-        # True if pursuer reaches evader
-        evader_caught = (x_p - x_e) ** 2 + (y_p - y_e) ** 2 <= self.d ** 2
-
-        # True if evader reaches defense area
-        evader_missed = (x_e - self.area_x) ** 2 + (y_e - self.area_y) ** 2 <= self.area_r ** 2
-
-        return evader_caught or evader_missed
+    @property
+    def evader_succeeds(self):
+        x_p, y_p = self.pursuer.get_position()
+        x_e, y_e = self.evader.get_position()
+        return  (x_e - self.area_x) ** 2 + (y_e - self.area_y) ** 2 <= self.area_r ** 2
     
-    def get_state(self, agent):
-        return agent.state
+    @property
+    def evader_cornered(self):
+        pass
+
+    @property
+    def is_terminal(self):
+        return self.pursuer_succeeds or self.evader_succeeds or self.evader_cornered
+    
+    @property
+    def get_state(self):
+        return self.pursuer.state
+    
+    @property
+    def get_reward(self):
+        if self.evader_succeeds:
+            return -10
+        elif self.pursuer_succeeds:
+            return 10
+        elif self.evader_cornered:
+            return 5
+
+        # d_tm1: previous distance between both agents
+        # d_t: current distance between both agents        
+        d_tm1 = 0
+        d_t = 0
+        return self.k * (d_tm1 - d_t)
