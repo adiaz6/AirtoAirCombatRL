06953bb7c324e7de050a2d9f085278a77c59d7ba
Alex <j.a.utexas@edu>
2024-04-06 16:09:07 -0500

    Implement step method and clean up method and property names

--- src/agent.py
+++ src/agent.py
@@ -33,7 +33,7 @@ class Pursuer():
         return s_dot
     
     @property
-    def get_position(self):
+    def position(self):
         return self.state[0], self.state[1]
     
 class Evader():
@@ -63,7 +63,7 @@ class Evader():
         s_dot[2] = 0 # sum of F_a and F_r (attraction force (vector pointing towards center of area), repulsive force (vector pointing away from pursuer))
     
     @property
-    def get_position(self):
+    def position(self):
         return self.state[0], self.state[1]
        
    
--- src/world.py
+++ src/world.py
@@ -8,7 +8,8 @@ class World(object):
                  v_min, v_max, u1_max, u2_max, 
                  start_state_pursuer,
                  start_state_evader,
-                 k):
+                 k, 
+                 dt):
         self.x_l = x_l # x lower limit
         self.x_u = x_u # x upper limit
         self.y_l = y_l # y lower limit
@@ -22,36 +23,64 @@ class World(object):
 
         self.k = 1 # distance reward scaling factor
 
-        self.action_space = list()
+        self.dt = dt # time step
+
+        # Each action choice is [linear_accel_control, angular_accel_control]
+        self.action_space = { 0: [0, 0],            # [0 linear accel, 0 angular accel] 
+                              1: [0, u2_max],       # [0 linear accel, max angular accel]
+                              2: [0, -u2_max],      # [0 linear accel, -max angular accel]
+                              3: [u1_max, 0],       # [max linear accel, 0 angular accel]
+                              4: [u1_max, u2_max],  # [max linear accel, max angular accel]
+                              5: [u1_max, -u2_max], # [max linear accel, -max angular accel]
+                              6: [-u1_max, 0],      # [-max linear accel, 0 angular accel]
+                              7: [-u1_max, u2_max],   # [-max linear accel, max angular accel]
+                              8: [-u1_max, -u2_max] } # [-max linear accel, -max angular accel]
+        
+        self.possible_actions = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         self.pursuer = Pursuer(start_state_pursuer, v_min, v_max, u1_max, u2_max)
         self.evader = Evader(start_state_evader, v_min, v_max, u1_max, u2_max)
 
+    # Distance between pursuer and evader
+    @property
+    def distance_pe(self):
+        x_p, y_p = self.pursuer.position
+        x_e, y_e = self.evader.position
+        return np.sqrt((x_p - x_e) ** 2 + (y_p - y_e) ** 2)
+    
+    # Distance between evader and target area
+    @property
+    def distance_et(self):
+        x_e, y_e = self.evader.position
+        return np.sqrt((x_e - self.area_x) ** 2 + (y_e - self.area_y) ** 2)
+    
+    # Check if pursuer has reached evader
     @property
     def pursuer_succeeds(self):
-        x_p, y_p = self.pursuer.get_position()
-        x_e, y_e = self.evader.get_position()
-        return (x_p - x_e) ** 2 + (y_p - y_e) ** 2 <= self.d ** 2
+        return self.distance_pe <= self.d
 
+    # Check if evader has reached target area
     @property
     def evader_succeeds(self):
-        x_p, y_p = self.pursuer.get_position()
-        x_e, y_e = self.evader.get_position()
-        return  (x_e - self.area_x) ** 2 + (y_e - self.area_y) ** 2 <= self.area_r ** 2
+        return self.distance_et <= self.area_r
     
+    # Check if evader has been chased beyond target area (outside of boundaries)
     @property
     def evader_cornered(self):
-        pass
+        x, y = self.evader.get_position
+        return x >= self.x_u or x <= self.x_l or y >= self.y_u or y <= self.y_l
 
+    # Check if state is terminal
     @property
     def is_terminal(self):
         return self.pursuer_succeeds or self.evader_succeeds or self.evader_cornered
     
+    # Get pursuer state
     @property
-    def get_state(self):
+    def state(self):
         return self.pursuer.state
     
-    @property
-    def get_reward(self):
+    # Get reward
+    def get_reward(self, dtm1):
         if self.evader_succeeds:
             return -10
         elif self.pursuer_succeeds:
@@ -61,6 +90,21 @@ class World(object):
 
         # d_tm1: previous distance between both agents
         # d_t: current distance between both agents        
-        d_tm1 = 0
-        d_t = 0
-        return self.k * (d_tm1 - d_t)
+        return self.k * (dtm1 - self.distance_pe)
+    
+    # Take a step according to some action
+    # Return new_state, reward, done, info (None)
+    def step(self, action):
+        dtm1 = self.distance_pe
+        self.pursuer.update_state(self.action_space[action])
+        self.evader.update_state()  
+
+        return self.pursuer.state, self.get_reward(dtm1), self.is_terminal, None
+    
+    # Initialize everything randomly
+    def reset(self):
+        pass
+
+    # Render 
+    def render(self):
+        pass
